8919317a0c4520e7794f5d5444ca36e046ba166a
Victor <victor@ananias.dev>
2020-08-15 15:52:14 -0300

    lets's migrate to graphql

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-import Github from 'github-api'
+const Github = require('github-api')
 
-const github = new Github()
+const token = 'bf438a3782dcf8a178376a64935ec812f279f913'
+const username = 'victorananias'
+
+const github = new Github({ token })
+
+const user = github.getUser()
+
+const name = '',
+  avatarUrl = '',
+  projects = [],
+  bio = '',
+  company = '',
+  programmingLanguages = [],
+  topics = [],
+  contact = [];
+
+// user.getProfile()
+//   .then(({ data }) => {
+//     const {
+//       name,
+//       avatar_url: avatarUrl,
+//       html_url: githubUrl,
+//       bio,
+//       location,
+//       blog,
+//       email,
+//       company
+//     } = data
+//     // console.log(Object.keys(data).join('\n'));
+//     console.log(data);
+//   })
+//   .catch(e => {
+//     console.log(e)
+//   })
+
+user.listRepos()
+  .then(({ data }) => {
+    // const names = data.map(r => r.name).join(', ')
+    console.log(data[0]);
+  })
+  .catch(e => {
+    console.error(e)
+  })
